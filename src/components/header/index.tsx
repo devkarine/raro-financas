bd3bcd8c6bbbf82522bef348029096b7af846535
karine <karinedeveloper@gmail.com>
2023-08-12 20:56:05 -0300

    adiciona header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,20 @@
+import './index.css'
+
+export const Header = () =>{
+    return(
+        <header className='header'>
+        <div className="logo">
+            <img src="src/assets/Icon/Outline/credit-card.svg" alt="Icone de cartão de crédito" />
+            <h1>Raro Finanças</h1>
+        </div>
+        <div className="user">
+            <div className="user-name-email">
+                <p className='name'>Karine Pereira</p>
+                <p className='email'>karinedeveloper@gmail.com</p>
+            </div>
+            <img src="src/assets/Icon/Outline/PROFILE.png" alt="Profile" />
+        </div>
+    </header>
+    )
+    
+}
