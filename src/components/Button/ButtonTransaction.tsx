789d69d030be57913fd0cd72b4f6bb780b236c0f
karine <karinedeveloper@gmail.com>
2023-08-14 13:56:36 -0300

    adiciona modal

--- src/components/Button/ButtonTransaction.tsx
+++ src/components/Button/ButtonTransaction.tsx
@@ -1,21 +1,18 @@
 import './ButtonTransaction.css';
-import { useState } from "react";
-import { Modal } from '../Modal/Modal';
 
 
 
 export const BtnTransaction = () => {
-    const [isModalVisible, setIsModalVisible] = useState(false);
+    
     return(
         
         <>
-            <button onClick= {() => setIsModalVisible(true)} className="btn-transaction">
+            <button className="btn-transaction">
                <figure>
                 <img src="src/assets/plus-sm.svg" alt="Sinal de soma" />
                </figure>
                 TRANSAÇÃO
             </button>
-            {isModalVisible ? <Modal/> : null}
         </>
     )
 }
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,13 +1,44 @@
 import './index.css';
+import { BtnModal } from '../Button/BtnModal';
+import { Inputs } from '../Inputs/Input';
+import { useState } from "react";
+
 
 
 export const Modal = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const closeModal = () => {
+    setIsModalVisible(false);
+    
+  };
+
   return (
     <>
-      <div className="modal">
+      <div className={`modal ${isModalVisible && 'open'}`}>
+        <div className="wrapper-modal">
         <form >
+          <h2>Nova Transação</h2>
 
+          <Inputs/>
+          
+
+          <div className="btns">
+          <BtnModal
+              nome="Cancelar"
+              img="x.svg"
+              background="#F1F5F9"
+              color="#1E40AF"
+              onClick={closeModal}
+            />
+            {isModalVisible ? <Modal/> : null}
+            <BtnModal nome='Adicionar' img='check.svg' background='#1E40AF' color='#F1F5F9' />
+          </div>
+          
+
+            
         </form>
+        </div>
+        
       </div>
     </>
   );
